08adc48df5268d5769cac7130b56ed8aff63c6dc
Ronney Vargas <ronneyvargas@gmail.com>
2021-08-10 16:00:18 -0500

    Added owl carousel

--- scripts.js
+++ scripts.js
@@ -1,6 +1,26 @@
 // Owl Carousel
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel();
+$('.owl-carousel').owlCarousel({
+    center: true,
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true,
+            dots: true,
+        },
+        600: {
+            items: 2,
+            nav: true,
+            dots: true,
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            dots: true,
+        }
+    }
 });
 
 const sideImages = [
@@ -9,7 +29,7 @@ const sideImages = [
 
 for (let i = 0; i < sideImages.length; i++) {
     const element = sideImages[i];
-    
+
     let imageContainer = document.getElementById('image-container')
     imageContainer.innerHTML += `<img class="clickable-image" src="/images/${element}" alt="${element}">`
 }
@@ -21,7 +41,7 @@ for (let i = 0; i < selectedImage.length; i++) {
     element.addEventListener('click', (e) => {
         e.preventDefault()
         let modalImg = document.querySelector('.modal-image-container')
-        // Get the modal to display larger image
+            // Get the modal to display larger image
         let modal = document.getElementById('myModal')
         let captionText = document.getElementById('caption')
         modal.style.display = 'block'
@@ -35,8 +55,7 @@ for (let i = 0; i < selectedImage.length; i++) {
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = () => { 
+span.onclick = () => {
     let modal = document.getElementById('myModal')
     modal.style.display = "none";
-}   
-  
+}
